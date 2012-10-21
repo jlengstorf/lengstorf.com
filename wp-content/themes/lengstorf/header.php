2a6d1c56467945f6bdf79f3b7e0c0d73100888f3
jlengstorf <jason.lengstorf@copterlabs.com>
2012-10-20 23:51:42 -0700

    Added the header markup.

---
+++ wp-content/themes/lengstorf/header.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and the header
+ *
+ * @package    WordPress
+ * @subpackage lengstorf.com
+ * @since      1.0
+ */
+
+$theme_dir = get_bloginfo('stylesheet_directory');
+$stylesheet_url = get_bloginfo('stylesheet_url') . '?' 
+                . filemtime(get_stylesheet_directory() . '/style.css');
+
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+
+<meta charset="<?php bloginfo('charset'); ?>" />
+<title><?php wp_title(''); ?></title>
+
+<link type="text/css" rel="stylesheet" 
+      href="http://fast.fonts.com/cssapi/c265c53b-e4e5-4941-9e4e-931e0009df73.css" />
+<link rel="stylesheet" type="text/css" 
+      href="<?php echo $stylesheet_url; ?>" />
+<link rel="pingback" 
+      href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+<?php wp_head(); ?> 
+</head>
+
+<body <?php body_class(); ?>>
+
+    <header>
+
+        <a href="<?php echo home_url('/'); ?>"
+           id="floating-head" rel="home" 
+           title="<?php bloginfo('name'); ?> &mdash; <?php bloginfo('description'); ?>">
+            <img src="<?php echo $theme_dir; ?>/assets/images/floating-head.png" 
+                 alt="Jason Lengstorf" />
+        </a><!--/#floating-head-->
+
+        <a href="<?php echo home_url('/'); ?>"
+           id="site-title" rel="home">
+            <?php bloginfo('name'); ?> 
+        </a><!--/#site-title-->
+
+        <p id="site-tagline">
+            <?php bloginfo('description'); ?> 
+        </p><!--/#site-tagline-->
+
+        <?php rw_social_links(); ?> 
+
+    </header>
+
+    <section id="content-wrapper">
+
