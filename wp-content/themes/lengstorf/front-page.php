375b7ba274c96cefa2d65aa6a0eca936ed7d2bcd
jlengstorf <jason.lengstorf@copterlabs.com>
2012-10-20 23:47:49 -0700

    Added the home page markup. Needs to be hooked up to WordPress for content.

---
+++ wp-content/themes/lengstorf/front-page.php
@@ -0,0 +1,66 @@
+<?php get_header(); ?> 
+
+        <article class="home-blurb">
+            <p>
+                Labore brooklyn fap pinterest pariatur, freegan butcher 
+                etsy duis voluptate ad locavore ut. Commodo nihil voluptate, 
+                yr keytar cliche bicycle rights beard adipisicing laboris 
+                aliquip scenester williamsburg. Hella qui bespoke aesthetic 
+                forage, godard fap fixie. Dolor scenester exercitation 
+                farm-to-table tofu, authentic voluptate wes anderson you 
+                probably haven't heard of them typewriter high life 
+                post-ironic lomo vice. +1 sapiente vinyl food truck iphone.
+            </p>
+            <a href="http://www.reddit.com/r/portland"
+               id="portland-or">
+               <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/portland-or.png" 
+                    alt="Portland, OR" />
+            </a>
+        </article><!--/.home-blurb-->
+
+        <ul id="my-stuff">
+            <li class="copter-link">
+                <h3>I run <span title="Copter Labs">this</span></h3>
+                <p>
+                    <a href="http://www.copterlabs.com?ref=jlcom">
+                        <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/copter-labs.png" 
+                             alt="Copter Labs" />
+                    </a>
+                </p>
+            </li><!--/.copter-link-->
+            <li class="random-art">
+                <h3>I made this</h3>
+                <img src="http://placekitten.com/200/201" 
+                     alt="kitties!" />
+            </li><!--/.random-art-->
+            <li class="book-links">
+                <h3>I wrote these</h3>
+                <ul>
+                    <li>
+                        <a href="http://www.amazon.com/gp/product/1430224738/ref=as_li_ss_tl?ie=UTF8&tag=copterlabs-20&linkCode=as2&camp=1789&creative=390957&creativeASIN=1430224738"
+                           class="book-pfab">
+                            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/book-pfab.png" 
+                                 alt="PHP for Absolute Beginners" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://www.amazon.com/gp/product/1430228474/ref=as_li_ss_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=1430228474&linkCode=as2&tag=copterlabs-20"
+                           class="book-ppaj">
+                            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/book-ppaj.png" 
+                                 alt="Pro PHP and jQuery" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://www.amazon.com/gp/product/1430246200/ref=as_li_ss_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=1430246200&linkCode=as2&tag=copterlabs-20"
+                           class="book-rtwa">
+                            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/book-rtwa.png" 
+                                 alt="Realtime Web Apps: HTML5 WebSocket, Pusher, and the Web&#146;s Next Big Thing" />
+                        </a>
+                    </li>
+                </ul>
+            </li><!--/.book-links-->
+        </ul><!--/#my-stuff-->
+
+<?php
+
+get_footer();
