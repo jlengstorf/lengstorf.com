831ab9ab82c3ca3530dc9ddc82ebcea5c9a664a6
jlengstorf <jason.lengstorf@copterlabs.com>
2013-03-03 23:25:41 -0800

    Added rel="external" to the book links.

--- wp-content/themes/lengstorf/front-page.php
+++ wp-content/themes/lengstorf/front-page.php
@@ -66,21 +66,21 @@ if (have_posts()):
                 <ul>
                     <li>
                         <a href="http://www.amazon.com/gp/product/1430224738/ref=as_li_ss_tl?ie=UTF8&tag=copterlabs-20&linkCode=as2&camp=1789&creative=390957&creativeASIN=1430224738"
-                           class="book-pfab">
+                           class="book-pfab" rel="external">
                             <img src="<?php echo $img_dir; ?>/book-pfab.jpg" 
                                  alt="PHP for Absolute Beginners" />
                         </a>
                     </li>
                     <li>
                         <a href="http://www.amazon.com/gp/product/1430228474/ref=as_li_ss_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=1430228474&linkCode=as2&tag=copterlabs-20"
-                           class="book-ppaj">
+                           class="book-ppaj" rel="external">
                             <img src="<?php echo $img_dir; ?>/book-ppaj.jpg" 
                                  alt="Pro PHP and jQuery" />
                         </a>
                     </li>
                     <li>
                         <a href="http://www.amazon.com/gp/product/1430246200/ref=as_li_ss_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=1430246200&linkCode=as2&tag=copterlabs-20"
-                           class="book-rtwa">
+                           class="book-rtwa" rel="external">
                             <img src="<?php echo $img_dir; ?>/book-rtwa.jpg" 
                                  alt="Realtime Web Apps: HTML5 WebSocket, Pusher, and the Web&#146;s Next Big Thing" />
                         </a>
