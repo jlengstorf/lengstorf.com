af666ab6949dd6d0b4fab91c9213acc8e024e6dc
jlengstorf <jason.lengstorf@copterlabs.com>
2013-06-14 13:19:23 -0700

    Updated the mobile sizing meta tag.

--- wp-content/themes/lengstorf/header.php
+++ wp-content/themes/lengstorf/header.php
@@ -21,8 +21,7 @@ $stylesheet_url = get_bloginfo('stylesheet_url') . '?'
 <meta charset="<?php bloginfo('charset'); ?>" />
 
 <!-- Mobile stuffs -->
-<meta name="viewport" 
-      content="width=device-width; initial-scale=1.0; minimum-scale=1; maximum-scale=1.0; user-scalable=0;" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <title><?php wp_title('&rsaquo;', TRUE, 'right'); ?></title>
 
