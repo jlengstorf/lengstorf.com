2e283a1440c0d8f59610eda3ef1084512eea5302
jlengstorf <jason.lengstorf@copterlabs.com>
2014-09-02 19:56:22 -0700

    feat(facebook-ad-tracking): Moved the Facebook ad tracking

--- wp-content/themes/lengstorf2014/header.php
+++ wp-content/themes/lengstorf2014/header.php
@@ -35,6 +35,25 @@ if (has_nav_menu('primary')) {
 
 <link rel="stylesheet" type="text/css" href="//cloud.typography.com/6102252/625342/css/fonts.css" />
 
+<?php if (is_single()): ?>
+<!-- Facebook Conversion Code for Blog Reads -->
+<script>(function() {
+  var _fbq = window._fbq || (window._fbq = []);
+  if (!_fbq.loaded) {
+    var fbds = document.createElement('script');
+    fbds.async = true;
+    fbds.src = '//connect.facebook.net/en_US/fbds.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(fbds, s);
+    _fbq.loaded = true;
+  }
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', '6020292706002', {'value':'0.00','currency':'USD'}]);
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6020292706002&amp;cd[value]=0.00&amp;cd[currency]=USD&amp;noscript=1" /></noscript>
+<?php endif; ?>
+
 <?php wp_head(); ?>
 
 </head>
