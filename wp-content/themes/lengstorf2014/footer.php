1a178c84ed2919ebd17237e429ee1e4ecd5c3946
jlengstorf <jason.lengstorf@copterlabs.com>
2014-09-02 19:52:42 -0700

    feat(fb-ad-tracking): added a Facebook ad tracking pixel

--- wp-content/themes/lengstorf2014/footer.php
+++ wp-content/themes/lengstorf2014/footer.php
@@ -67,6 +67,23 @@ $site_credit = rw_footer_credit_link("Site by Copter Labs");
     </div>
 </footer>
 
+<!-- Facebook Conversion Code for Blog Reads -->
+<script>(function() {
+  var _fbq = window._fbq || (window._fbq = []);
+  if (!_fbq.loaded) {
+    var fbds = document.createElement('script');
+    fbds.async = true;
+    fbds.src = '//connect.facebook.net/en_US/fbds.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(fbds, s);
+    _fbq.loaded = true;
+  }
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', '6020292706002', {'value':'0.00','currency':'USD'}]);
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6020292706002&amp;cd[value]=0.00&amp;cd[currency]=USD&amp;noscript=1" /></noscript>
+
 <?php wp_footer(); ?>
 </body>
 </html>
