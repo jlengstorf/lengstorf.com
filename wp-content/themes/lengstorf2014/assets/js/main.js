86fd8df67a2ab80f68d0479fa70ef8b7b4f37d95
jlengstorf <jason.lengstorf@copterlabs.com>
2014-11-07 16:22:19 -0800

    feat(affix): haiku
    fixed the top offset
    I am a leaf in the wind
    I go with the flow

--- wp-content/themes/lengstorf2014/assets/js/main.js
+++ wp-content/themes/lengstorf2014/assets/js/main.js
@@ -33,7 +33,7 @@ var size = window.getComputedStyle(document.body,':after')
         if ($meta.length>0) {
             $meta.affix({
                 offset: {
-                    top: $meta.offset().top-50
+                    top: $meta.offset().top-65
                 }
             });
         }
