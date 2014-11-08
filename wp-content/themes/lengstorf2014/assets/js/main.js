12e6ef050ed552b6c2df68b5e21ddc6293eed463
jlengstorf <jason.lengstorf@copterlabs.com>
2014-11-07 16:17:11 -0800

    feat(affix): trying to get fancy...

--- wp-content/themes/lengstorf2014/assets/js/main.js
+++ wp-content/themes/lengstorf2014/assets/js/main.js
@@ -33,7 +33,7 @@ var size = window.getComputedStyle(document.body,':after')
         if ($meta.length>0) {
             $meta.affix({
                 offset: {
-                    top: $meta.offset().top-90
+                    top: $meta.offset().top-10
                 }
             });
         }
