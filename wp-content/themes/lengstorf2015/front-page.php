95ec43e3c798743468b18bfc7636acd0657ca58f
jlengstorf <jason.lengstorf@copterlabs.com>
2015-03-24 17:49:35 +0700

    feat(about): removed footer about — moved to footer.php

--- wp-content/themes/lengstorf2015/front-page.php
+++ wp-content/themes/lengstorf2015/front-page.php
@@ -62,45 +62,6 @@ get_header();
         </div>
     </div>
 
-    <div class="home-about">
-        <div class="home-about__image">
-            <img src="http://localhost:5000/wp-content/themes/lengstorf2015/assets/images/jason-lengstorf_footer-sm.jpg" 
-                 alt="Jason Lengstorf — Author, Speaker, Digital Nomad">
-        </div>
-        <div class="home-about__bio">
-            <h2 class="home-about__headline">
-                I&rsquo;m Jason Lengstorf: an author, speaker, and digital nomad.
-            </h2>
-            <div class="home-about__copy">
-                <p>
-                    I&rsquo;m also an Overkill Cult survivor.
-                    In 2013 I was working 70&ndash;90 hours a week &mdash; it got 
-                    so bad that patches of my beard started falling out. I needed to make some changes to my lifestyle before it killed me.
-                </p>
-                <p>
-                    In 2014 I sold everything I owned and started traveling the 
-                    world. I thought it would be hard, but it&rsquo;s been 
-                    surprisingly easy to work remotely from places like Barcelona, 
-                    London, and Chiang Mai.
-                </p>
-                <p>
-                    What I'm doing is exciting, but I'm not special. <em>Anyone</em> 
-                    can do this. If you've ever dreamed of working remotely and 
-                    exploring the world, I want to help you make your dream a 
-                    reality.
-                </p>
-            </div>
-            <div class="home-about__links">
-                <ul>
-                    <li><a href="#">About Jason</a></li>
-                    <li><a href="#">Facebook</a></li>
-                    <li><a href="#">Twitter</a></li>
-                    <li><a href="#">Instagram</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-
 <?php
 
 get_footer();
