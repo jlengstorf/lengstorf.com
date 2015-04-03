411d1c1b840bdfeb97709f3aa92bdf02e8e9d6f2
jlengstorf <jason.lengstorf@copterlabs.com>
2015-04-03 13:01:45 +0700

    feat(home): swapped home and alt home templates

--- wp-content/themes/lengstorf2015/front-page.php
+++ wp-content/themes/lengstorf2015/front-page.php
@@ -1,65 +1,87 @@
 <?php
 
+/*
+ * Template Name: Front Page Alt
+ */
+
 get_header();
 
 ?>
 
-    <div class="hometown-hero">
+    <div class="hometown-hero--alt">
         <h1 class="hometown-hero__headline">
-            I&rsquo;ve spent the last
-            <strong class="days-abroad">few months</strong>
-            <em>
-                traveling the world<br>
-                &amp; working remotely.
-            </em>
-            I want to show you
-            <strong class="how-i-did-it">how I did it.</strong>
-            <em>(And how you can, too.)</em>
+            You Can Work from
+            <strong>Anywhere</strong>
+            <em>I&rsquo;ll Show You How</em>
         </h1>
-        <form action=""
-              class="hometown-hero__opt-in">
-            <label for="email" 
-                   class="hometown-hero__opt-in-label">Email Address</label>
-            <input type="email" class="hometown-hero__opt-in-input" id="email">
-            <button class="hometown-hero__opt-in-submit">Get the Guide</button>
-        </form>
+        <a href="<?= home_url('/remote-work-course/') ?>"
+           class="hometown-hero__button">Get the Guide</a>
     </div>
 
-    <div class="cta">
+    <div class="cta--alt">
         <h2 class="cta__headline">
-            Stop thinking about the way you wish life could be.
-            <strong>Start living your dream life today.</strong>
+            For
+            <span class="days-abroad">months</span>
+            I&rsquo;ve been traveling the world &amp; working remotely.
+            <strong class="how-i-did-it">I want to show you how I did it.</strong>
+            <em>(And how you can, too.)</em>
         </h2>
-        <div class="cta__call work-remotely">
+        <div class="cta__call break-free">
             <h3 class="cta__subhead">
-                <a href="<?= home_url('/remote-work/') ?>">Work from Anywhere.</a>
+                <a href="<?= home_url('/remote-work/') ?>">Break Free of the Cult.</a>
             </h3>
             <p class="cta__tagline">
-                You can already make your living online. Why not do it from 
-                somewhere you&rsquo;ve always wanted to visit?
+                Learn how offices are run more like a cult than a 
+                healthy workplace — and how to escape.
             </p>
-            <a href="<?= home_url('/remote-work/') ?>" class="cta__link">Start Working from Anywhere</a>
         </div>
-        <div class="cta__call remote-productivity">
+        <div class="cta__call know-options">
             <h3 class="cta__subhead">
-                <a href="<?= home_url('/remote-productivity/') ?>">Get More Done Faster.</a>
+                <a href="<?= home_url('/remote-productivity/') ?>">Know Your Options.</a>
             </h3>
             <p class="cta__tagline">
-                What good is traveling if you don&rsquo;t get out 
-                and explore? Work smart. Finish early. Go adventure.
+                See the dozens of options for anyone who  
+                wants an alternative to the 40-hour lifestyle.
             </p>
-            <a href="<?= home_url('/remote-productivity/') ?>" class="cta__link">Get Remote Productivity Tips</a>
         </div>
-        <div class="cta__call happiness-101">
+        <div class="cta__call permission">
             <h3 class="cta__subhead">
-                <a href="<?= home_url('/happiness/') ?>">Truly Enjoy Your Life.</a>
+                <a href="<?= home_url('/happiness/') ?>">Give Yourself Permission.</a>
             </h3>
             <p class="cta__tagline">
-                Life isn&rsquo;t worth much if we spend all our time miserable.
-                Find your smile again and live well.
+                Identify the forces holding you back from 
+                what you want. (Hint: the worst one is you.)
             </p>
-            <a href="<?= home_url('/happiness/') ?>" class="cta__link">Do Some Happiness Practice</a>
         </div>
+        <div class="cta__call remove-anchors">
+            <h3 class="cta__subhead">
+                <a href="<?= home_url('/remote-work/') ?>">Remove Your Anchors.</a>
+            </h3>
+            <p class="cta__tagline">
+                Learn the common traps we fall into that 
+                anchor us in place — and how to remove them.
+            </p>
+        </div>
+        <div class="cta__call action-plan">
+            <h3 class="cta__subhead">
+                <a href="<?= home_url('/remote-productivity/') ?>">Design an Action Plan.</a>
+            </h3>
+            <p class="cta__tagline">
+                Put together a concrete, clear todo list and 
+                prepare for your newly-upgraded lifestyle.
+            </p>
+        </div>
+        <div class="cta__call begin-life">
+            <h3 class="cta__subhead">
+                <a href="<?= home_url('/happiness/') ?>">Begin Your New Life.</a>
+            </h3>
+            <p class="cta__tagline">
+                Take the first steps toward becoming happier,
+                healthier, and location-independent.
+            </p>
+        </div>
+        <a href="<?= home_url('/remote-work-course/') ?>"
+           class="cta__button">Download the Guide Free</a>
     </div>
 
 <?php
--- wp-content/themes/lengstorf2015/template-front-page-alt.php
+++ wp-content/themes/lengstorf2015/template-front-page-alt.php
@@ -1,87 +1,65 @@
 <?php
 
-/*
- * Template Name: Front Page Alt
- */
-
 get_header();
 
 ?>
 
-    <div class="hometown-hero--alt">
+    <div class="hometown-hero">
         <h1 class="hometown-hero__headline">
-            You Can Work from
-            <strong>Anywhere</strong>
-            <em>I&rsquo;ll Show You How</em>
+            I&rsquo;ve spent the last
+            <strong class="days-abroad">few months</strong>
+            <em>
+                traveling the world<br>
+                &amp; working remotely.
+            </em>
+            I want to show you
+            <strong class="how-i-did-it">how I did it.</strong>
+            <em>(And how you can, too.)</em>
         </h1>
-        <a href="<?= home_url('/remote-work-course/') ?>"
-           class="hometown-hero__button">Get the Guide</a>
+        <form action=""
+              class="hometown-hero__opt-in">
+            <label for="email" 
+                   class="hometown-hero__opt-in-label">Email Address</label>
+            <input type="email" class="hometown-hero__opt-in-input" id="email">
+            <button class="hometown-hero__opt-in-submit">Get the Guide</button>
+        </form>
     </div>
 
-    <div class="cta--alt">
+    <div class="cta">
         <h2 class="cta__headline">
-            For
-            <span class="days-abroad">months</span>
-            I&rsquo;ve been traveling the world &amp; working remotely.
-            <strong class="how-i-did-it">I want to show you how I did it.</strong>
-            <em>(And how you can, too.)</em>
+            Stop thinking about the way you wish life could be.
+            <strong>Start living your dream life today.</strong>
         </h2>
-        <div class="cta__call break-free">
+        <div class="cta__call work-remotely">
             <h3 class="cta__subhead">
-                <a href="<?= home_url('/remote-work/') ?>">Break Free of the Cult.</a>
+                <a href="<?= home_url('/remote-work/') ?>">Work from Anywhere.</a>
             </h3>
             <p class="cta__tagline">
-                Learn how offices are run more like a cult than a 
-                healthy workplace — and how to escape.
+                You can already make your living online. Why not do it from 
+                somewhere you&rsquo;ve always wanted to visit?
             </p>
+            <a href="<?= home_url('/remote-work/') ?>" class="cta__link">Start Working from Anywhere</a>
         </div>
-        <div class="cta__call know-options">
+        <div class="cta__call remote-productivity">
             <h3 class="cta__subhead">
-                <a href="<?= home_url('/remote-productivity/') ?>">Know Your Options.</a>
+                <a href="<?= home_url('/remote-productivity/') ?>">Get More Done Faster.</a>
             </h3>
             <p class="cta__tagline">
-                See the dozens of options for anyone who  
-                wants an alternative to the 40-hour lifestyle.
+                What good is traveling if you don&rsquo;t get out 
+                and explore? Work smart. Finish early. Go adventure.
             </p>
+            <a href="<?= home_url('/remote-productivity/') ?>" class="cta__link">Get Remote Productivity Tips</a>
         </div>
-        <div class="cta__call permission">
+        <div class="cta__call happiness-101">
             <h3 class="cta__subhead">
-                <a href="<?= home_url('/happiness/') ?>">Give Yourself Permission.</a>
+                <a href="<?= home_url('/happiness/') ?>">Truly Enjoy Your Life.</a>
             </h3>
             <p class="cta__tagline">
-                Identify the forces holding you back from 
-                what you want. (Hint: the worst one is you.)
+                Life isn&rsquo;t worth much if we spend all our time miserable.
+                Find your smile again and live well.
             </p>
+            <a href="<?= home_url('/happiness/') ?>" class="cta__link">Do Some Happiness Practice</a>
         </div>
-        <div class="cta__call remove-anchors">
-            <h3 class="cta__subhead">
-                <a href="<?= home_url('/remote-work/') ?>">Remove Your Anchors.</a>
-            </h3>
-            <p class="cta__tagline">
-                Learn the common traps we fall into that 
-                anchor us in place — and how to remove them.
-            </p>
-        </div>
-        <div class="cta__call action-plan">
-            <h3 class="cta__subhead">
-                <a href="<?= home_url('/remote-productivity/') ?>">Design an Action Plan.</a>
-            </h3>
-            <p class="cta__tagline">
-                Put together a concrete, clear todo list and 
-                prepare for your newly-upgraded lifestyle.
-            </p>
-        </div>
-        <div class="cta__call begin-life">
-            <h3 class="cta__subhead">
-                <a href="<?= home_url('/happiness/') ?>">Begin Your New Life.</a>
-            </h3>
-            <p class="cta__tagline">
-                Take the first steps toward becoming happier,
-                healthier, and location-independent.
-            </p>
-        </div>
-        <a href="<?= home_url('/remote-work-course/') ?>"
-           class="cta__button">Download the Guide Free</a>
     </div>
 
 <?php
