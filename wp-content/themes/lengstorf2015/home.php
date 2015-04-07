7cd49c94ee284cdff80988ee54a4b5fd78f52dea
jlengstorf <jason.lengstorf@copterlabs.com>
2015-04-07 18:17:49 +0700

    feat(blog): better blog page title

--- wp-content/themes/lengstorf2015/home.php
+++ wp-content/themes/lengstorf2015/home.php
@@ -4,7 +4,7 @@ get_header();
 
 ?>
     <div class="main-content post-archive">
-        <h1 class="main-content__archive-title">The Blog of Jason Lengstorf</h1>
+        <h1 class="main-content__archive-title">Recent Blog Posts</h1>
 <?php
 
 get_template_part('loop');
