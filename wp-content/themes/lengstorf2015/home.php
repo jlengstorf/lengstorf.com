8c9a1e3a2cd276e8d2888c23765ccb2971b3eff3
jlengstorf <jason.lengstorf@copterlabs.com>
2015-04-07 18:15:47 +0700

    feat(blog): changed blog page H1

--- wp-content/themes/lengstorf2015/home.php
+++ wp-content/themes/lengstorf2015/home.php
@@ -4,7 +4,7 @@ get_header();
 
 ?>
     <div class="main-content post-archive">
-        <h1 class="main-content__archive-title">Posts by Date</h1>
+        <h1 class="main-content__archive-title">The Blog of Jason Lengstorf</h1>
 <?php
 
 get_template_part('loop');
