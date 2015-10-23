e1d8b63469dfb42d5a0553adcf0c3e7c0d5b3873
jlengstorf <jason.lengstorf@copterlabs.com>
2015-10-23 15:11:34 +0700

    feat(deploy): added invalidations to the deployment process
    The deployment script now creates a CloudFront invalidation to
    speed up the CDN refresh for new content.

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,9 @@ hugo -t lengstorf
 # syncs all HTML, XML, CSS, JS, JPG, PNG, and GIF files + removes unused files
 aws s3 sync ./public s3://lengstorf.com --cache-control "max-age=86400" --exclude "*" --include "*.html" --include "*.xml"  --include "*.js" --include "*.css" --include "*.png" --include "*.jpg" --include "*.gif" --delete
 
+# creates a CloudFront invalidation batch for the deploy
+./invalidate.sh "/*"
+
 # --------------------------------------------------------------------
 # VARIOUS AWS COMMANDS
 #
