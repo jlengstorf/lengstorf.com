3a0c980ff26ab3c8743e469475494b24562042fc
jlengstorf <jason.lengstorf@copterlabs.com>
2016-08-04 16:44:30 +0200

    fix(deploy): increased the cache length for S3

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ rm -rf public/
 # generates a fresh copy of the site
 hugo
 
-# syncs all HTML, XML, CSS, JS, JPG, PNG, and GIF files + removes unused files
-aws s3 sync ./public s3://lengstorf.com --cache-control "max-age=86400" --exclude "*" --include "*.html" --include "*.pdf" --include "*.xml"  --include "*.js" --include "*.css" --include "*.png" --include "*.jpg" --include "*.gif" --delete
+# syncs files with 30-day cache expiration + removes unused files
+aws s3 sync ./public s3://lengstorf.com --cache-control "max-age=2592000" --exclude "*" --include "*.html" --include "*.pdf" --include "*.xml"  --include "*.js" --include "*.css" --include "*.png" --include "*.svg" --include "*.ico" --include "*.jpg" --include "*.json" --include "*.txt" --include "*.gif" --delete
 
 # creates a CloudFront invalidation batch for the deploy
 ./invalidate.sh "/*"
