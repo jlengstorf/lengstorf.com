8d3f0c62e3fde7089598bf993d6f073d83a2b882
Jason Lengstorf <jason@lengstorf.com>
2017-12-02 10:07:55 -0600

    feat: 2 years in Airbnbs + page stubs
    - Stubs for CTA pages to prevent links from breaking
    - Added .editorconfig to make Markdown cleanup less painful

---
+++ src/pages/productivity-habits.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import Layout from '../components/Layout';
+
+const ProductivityHabits = () => (
+  <Layout title="TKTK PRODUCTIVITY HABITS">
+    <h1>PRODUCTIVITY HABITS</h1>
+  </Layout>
+);
+
+export default ProductivityHabits;
---
+++ src/pages/work-happier.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import Layout from '../components/Layout';
+
+const WorkHappier = () => (
+  <Layout title="TKTK WORK HAPPIER">
+    <h1>WORK HAPPIER</h1>
+  </Layout>
+);
+
+export default WorkHappier;
