df3c67498435d4236a8cd5261253a605f59a768e
Jason Lengstorf <jason@lengstorf.com>
2017-12-08 23:23:04 -0600

    feat: post formatting

---
+++ src/pages/speaking.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import Layout from '../components/Layout';
+
+const Speaking = () => (
+  <Layout title="TKTK SPEAKING">
+    <h1>SPEAKING</h1>
+  </Layout>
+);
+
+export default Speaking;
