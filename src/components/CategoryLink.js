6eca582be9d8269362f52535c408a1c211057388
Jason Lengstorf <jason@lengstorf.com>
2018-01-04 16:58:33 -0600

    fix: style cleanup

--- src/components/CategoryLink.js
+++ src/components/CategoryLink.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import config from '../config';
 import styles from '../styles/category-link.module.css';
 
-export default ({ category, block }) => (
+const CategoryLink = ({ category, block }) => (
   <Link
     to={`/blog/category/${category}`}
-    className={`${styles.link} ${block && styles['link--block']}`}
+    className={`${styles.link} ${block && styles.linkBlock}`}
   >
     {config.categories[category]
       ? config.categories[category].display
       : category}
   </Link>
 );
+
+CategoryLink.propTypes = {
+  category: PropTypes.string.isRequired,
+  block: PropTypes.bool,
+};
+
+CategoryLink.defaultProps = {
+  block: false,
+};
+
+export default CategoryLink;
