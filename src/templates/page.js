376f44bb0781bc5fdcde0232c1edc828c1abe813
Jason Lengstorf <jason@lengstorf.com>
2018-12-10 15:32:34 +0100

    feat: add support for SEO images

--- src/templates/page.js
+++ src/templates/page.js
@@ -2,12 +2,32 @@ import React from 'react';
 import Layout from '../components/Layout';
 import ContentWithFootnotes from '../components/ContentWithFootnotes';
 import ContentArea from '../components/ContentArea';
+import SEO from '../components/SEO/SEO';
 
-export default ({ children, pageContext }) => (
-  <Layout title="Test">
-    <h1>{pageContext.frontmatter.title}</h1>
-    <ContentWithFootnotes
-      render={() => <ContentArea>{children}</ContentArea>}
-    />
-  </Layout>
-);
+export default ({ children, pageContext, data }) => {
+  console.log(pageContext.frontmatter);
+  const postImage =
+    data &&
+    data.file &&
+    data.file.childImageSharp &&
+    data.file.childImageSharp.resize &&
+    data.file.childImageSharp.resize.src
+      ? data.file.childImageSharp.resize.src
+      : null;
+
+  const seo = {
+    frontmatter: pageContext.frontmatter,
+    postImage,
+  };
+
+  return (
+    <Layout title={pageContext.frontmatter.title}>
+      {console.log(data)}
+      <SEO {...seo} />
+      <h1>{pageContext.frontmatter.title}</h1>
+      <ContentWithFootnotes
+        render={() => <ContentArea>{children}</ContentArea>}
+      />
+    </Layout>
+  );
+};
