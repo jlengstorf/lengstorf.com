94f3a64ce6a57134e46eec98f7a2a25966506c3a
Jason Lengstorf <jason@lengstorf.com>
2019-02-08 09:03:41 -0800

    feat: add amplitude and manifest plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -158,7 +158,41 @@ module.exports = {
         modules: ['gatsby-theme-jason-blog']
       }
     },
-
+    {
+      resolve: 'gatsby-plugin-amplitude-analytics',
+      options: {
+        apiKey: process.env.AMPLITUDE_API_KEY,
+        head: false,
+        respectDNT: true,
+        amplitudeConfig: {
+          includeUtm: true,
+          includeReferrer: true,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Jason Lengstorf',
+        short_name: '@jlengstorf',
+        start_url: '.',
+        theme_color: '#c800ec',
+        background_color: '#ffffff',
+        display: 'minimal-ui',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png?v=6946GROn29',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png?v=6946GROn29',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    },
     // I give up. 😭
     'gatsby-plugin-remove-serviceworker',
     // {
